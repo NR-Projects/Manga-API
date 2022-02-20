1544991c90187a4fd22ddd6c555e7255f3df1653
Null-Root <triascend94@gmail.com>
2022-02-20 16:55:52 +0800

    Add: Consts

---
+++ manga_api/consts.py
@@ -0,0 +1,17 @@
+class Label:
+    MANGA_LINK = "Manga_Link"
+    MANGA_NAME = "Manga_Name"
+    MANGA_AUTHORS = "Manga_Authors"
+    MANGA_GENRES = "Manga_Genres"
+
+    class SFW:
+        MANGA_DESC = "Manga_Desc"
+        MANGA_PIC_LINK = "Manga_Pic_Link"
+        MANGA_LAST_UPDATED = "Manga_Last_Updated"
+        MANGA_TOTAL_CHAPTER_COUNT = "Manga_Total_Chapter_Count"
+        MANGA_LATEST_CHAPTER_NAME = "Manga_Latest_Chapter_Name"
+
+    class NSFW:
+        MANGA_PAGES = "Manga_Pages"
+        MANGA_PAGE_LINKS = "Manga_Page_Links"
+        MANGA_UPLOADED = "Manga_Uploaded"
