94bbfecb74f8a102a3938737c5c8b5df585f2811
Null-Root <triascend94@gmail.com>
2022-02-21 14:59:16 +0800

    Remove: __name__ == '__main__' check

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from manga_api.nsfw import nsfw
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.register_blueprint(sfw, url_prefix='/sfw')
-    app.register_blueprint(nsfw, url_prefix='/nsfw')
+app.register_blueprint(sfw, url_prefix='/sfw')
+app.register_blueprint(nsfw, url_prefix='/nsfw')
 
-    app.run()
+app.run()
