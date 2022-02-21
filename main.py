4a57689f98e65dccbf91aeed0bd2946caacc00c9
Null-Root <triascend94@gmail.com>
2022-02-21 14:44:03 +0800

    Remove: Extra function from main.py

--- main.py
+++ main.py
@@ -3,14 +3,10 @@ from flask import Flask
 from manga_api.sfw import sfw
 from manga_api.nsfw import nsfw
 
-def create_app():
+if __name__ == '__main__':
     app = Flask(__name__)
 
     app.register_blueprint(sfw, url_prefix='/sfw')
     app.register_blueprint(nsfw, url_prefix='/nsfw')
 
-    return app
-
-
-if __name__ == '__main__':
-    create_app().run()
+    app.run()
