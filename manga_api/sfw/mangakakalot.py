00b737c15625c8bf1034e5521b12d0605a700748
Null-Root <triascend94@gmail.com>
2022-02-21 08:53:04 +0800

    Add: Checks for Valid Links in Mangakakalot API

--- manga_api/sfw/mangakakalot.py
+++ manga_api/sfw/mangakakalot.py
@@ -10,56 +10,62 @@ mangakakalot_endpoint = Blueprint('mangakakalot', __name__)
 class Mangakakalot():
     def __init__(self, Url):
         self.Url = Url
-        self.Response = requests.get(self.Url, headers=Site.HEADERS)
+        
     
     def GetData(self) -> dict:
         MangaData = {}
 
-        # Set Manga Link
-        MangaData[Label.MANGA_LINK] = self.Url
+        try:
+            # Fetch Data from URL
+            self.Response = requests.get(self.Url, headers=Site.HEADERS)
 
-        full_manga_content = BeautifulSoup(self.Response.content, 'html.parser')
+            # Set Manga Link
+            MangaData[Label.MANGA_LINK] = self.Url
 
-        Infos = full_manga_content.find("ul", {"class":"manga-info-text"}).find_all("li")
-        Chapters_Container = full_manga_content.find("div", {"class": "chapter-list"})
+            full_manga_content = BeautifulSoup(self.Response.content, 'html.parser')
 
-        # Get Manga Name
-        Name = Infos[0].find('h1').get_text()
-        MangaData[Label.MANGA_NAME] = Name
+            Infos = full_manga_content.find("ul", {"class":"manga-info-text"}).find_all("li")
+            Chapters_Container = full_manga_content.find("div", {"class": "chapter-list"})
 
-        # Get Manga Authors
-        AuthorList = []
-        Authors = Infos[1].find_all("a")
-        for author in Authors:
-            AuthorList.append(author.get_text())
-        MangaData[Label.MANGA_AUTHORS] = AuthorList
+            # Get Manga Name
+            Name = Infos[0].find('h1').get_text()
+            MangaData[Label.MANGA_NAME] = Name
 
-        # Get Last Updated
-        LastUpdated = Infos[3].get_text()[len("Last_Updated : "):]
-        MangaData[Label.SFW.MANGA_LAST_UPDATED] = LastUpdated
+            # Get Manga Authors
+            AuthorList = []
+            Authors = Infos[1].find_all("a")
+            for author in Authors:
+                AuthorList.append(author.get_text())
+            MangaData[Label.MANGA_AUTHORS] = AuthorList
 
-        # Get Genres
-        GenreList = []
-        Genres = Infos[6].find_all("a")
-        for genre in Genres:
-            GenreList.append(genre.get_text())
-        MangaData[Label.MANGA_GENRES] = GenreList
+            # Get Last Updated
+            LastUpdated = Infos[3].get_text()[len("Last_Updated : "):]
+            MangaData[Label.SFW.MANGA_LAST_UPDATED] = LastUpdated
 
-        # Get Pic Link
-        PicLink = full_manga_content.find("div", {"class": "manga-info-pic"}).find("img")["src"]
-        MangaData[Label.MANGA_PIC_LINK] = PicLink
+            # Get Genres
+            GenreList = []
+            Genres = Infos[6].find_all("a")
+            for genre in Genres:
+                GenreList.append(genre.get_text())
+            MangaData[Label.MANGA_GENRES] = GenreList
 
-        # Get Desc
-        Desc = "".join(full_manga_content.find("div", {"id": "noidungm"}).findAll(text=True, recursive=False)).strip()
-        MangaData[Label.SFW.MANGA_DESC] = Desc
+            # Get Pic Link
+            PicLink = full_manga_content.find("div", {"class": "manga-info-pic"}).find("img")["src"]
+            MangaData[Label.MANGA_PIC_LINK] = PicLink
 
-        # Get Total Chapter Count
-        TotalChapterCount = len(Chapters_Container.find_all("div", {"class":"row"}))
-        MangaData[Label.SFW.MANGA_TOTAL_CHAPTER_COUNT] = TotalChapterCount
+            # Get Desc
+            Desc = "".join(full_manga_content.find("div", {"id": "noidungm"}).findAll(text=True, recursive=False)).strip()
+            MangaData[Label.SFW.MANGA_DESC] = Desc
 
-        # Get Latest Chapter Name
-        LatestChapterName = Chapters_Container.find_all("div", {"class":"row"})[0].find("span").find("a").get_text()
-        MangaData[Label.SFW.MANGA_LATEST_CHAPTER_NAME] = LatestChapterName
+            # Get Total Chapter Count
+            TotalChapterCount = len(Chapters_Container.find_all("div", {"class":"row"}))
+            MangaData[Label.SFW.MANGA_TOTAL_CHAPTER_COUNT] = TotalChapterCount
+
+            # Get Latest Chapter Name
+            LatestChapterName = Chapters_Container.find_all("div", {"class":"row"})[0].find("span").find("a").get_text()
+            MangaData[Label.SFW.MANGA_LATEST_CHAPTER_NAME] = LatestChapterName
+        except:
+            return None
 
         return MangaData
 
@@ -71,5 +77,8 @@ def index():
         return jsonify(Response=Response('failed', 'missing "/MangaLink"/ parameter', {}).GetData()), 400
 
     MangaData = Mangakakalot(RawMangaLink).GetData()
+
+    if MangaData is None:
+        return jsonify(Response=Response('failed', 'invalid "/MangaLink"/ parameter', {}).GetData()), 400
 
     return jsonify(Response=Response('success', '', MangaData).GetData()), 200
