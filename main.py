d571c47d74906887d4df13dee44902bc6a317e09
Null-Root <triascend94@gmail.com>
2022-02-21 14:49:34 +0800

    Refactor: main.py to place app in the global scope

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from flask import Flask
 from manga_api.sfw import sfw
 from manga_api.nsfw import nsfw
 
-if __name__ == '__main__':
-    app = Flask(__name__)
+app = Flask(__name__)
 
+if __name__ == '__main__':
     app.register_blueprint(sfw, url_prefix='/sfw')
     app.register_blueprint(nsfw, url_prefix='/nsfw')
 
